dfd32ac662c1b03d23f8cf066eae220b2992860a
amanners <amanners@mitre.org>
2019-10-29 08:15:31 -0400

    moving compile location

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -19,7 +19,7 @@ class SandService:
                 if param in headers:
                     ldflags.append('-X main.%s=%s' % (param, headers[param]))
 
-            output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
+            output = 'plugins/%s/data/payloads/%s-%s' % (plugin, name, platform)
             self.file_svc.log.debug('Dynamically compiling %s' % name)
             await self.file_svc.compile_go(platform, output, file_path, ldflags=' '.join(ldflags))
         return '%s-%s' % (name, platform)
